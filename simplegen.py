26253df67ec2bd9bd110a4b309fcd7498b184b28
mennymendoza <Juanmendoza97@mail.fresnostate.edu>
2021-03-31 19:40:45 -0700

    pycache folder added

--- simplegen.py
+++ simplegen.py
@@ -3,8 +3,9 @@ import random
 
 # some tests
 my_cube = cb.RCube()
-print(my_cube.calc_fit())
+print('Fitness:', my_cube.calc_fit())
 my_list = []
 for z in range(0, 5):
     my_list.append(random.randrange(0, 18))
 my_cube.run_list(my_list)
+print('Fitness:', my_cube.calc_fit())
