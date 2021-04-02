2ef643b29195b07f5ea10a91093640299d19a2ea
mennymendoza <Juanmendoza97@mail.fresnostate.edu>
2021-04-01 21:10:53 -0700

    finished sga outline

--- simplegen.py
+++ simplegen.py
@@ -1,35 +1,23 @@
 import rcube as cb
 import random
+import time
 
 # Constants
+NUM_SHUFFLES = 700
 LIST_SIZE = 50
 POPULATION_SIZE = 100
-NUM_ITER = 500
-RAND_RESET_PROB = 0.3
-
-# Select Parents
-def select_p(population):
-    parents = []
-    sum_f = 0
-    for g in population:
-        (f, _) = g
-        sum_f += f
-    for g in population:
-        (f, li) = g
-        prob = float(f / sum_f)
-        print(prob)
-        if (random.random() < prob):
-            parents.append(g)
-    return parents
-
-# Update Population
-def update_pop(population, childen):
-    new_pop = []
-    return new_pop
+NUM_ITER = 5
+RAND_RESET_PROB = 0.8
+PROP_CONSTANT = 0.9
 
 def gen_sga_sol():
-    # Initialization
+    # Cube Initialization
+    random.seed(time.time())
     my_cube = cb.RCube()
+    for _ in range(0, NUM_SHUFFLES):
+        my_cube.rotate(random.randrange(0, 18))
+    og_state = my_cube.cube_mat
+    my_cube.print_colors()
 
     # Generates Initial Population
     population = []
@@ -38,21 +26,43 @@ def gen_sga_sol():
         genotype = []
         for _ in range(0, LIST_SIZE):
             genotype.append(random.randrange(0, 18))
+        my_cube.cube_mat = og_state
         population.append((my_cube.run_list(genotype), genotype))
 
-    # Select Parents
-    parents = select_p(population)
-    print(parents)
+    for _ in range(0, NUM_ITER):
+        # Select Parents
+        parents = []
+        sum_f = 0
+        for g in population:
+            (f, _) = g
+            sum_f += f
+        for g in population:
+            (f, _) = g
+            prob = float(f / sum_f)
+            if (PROP_CONSTANT*random.random() < prob):
+                parents.append(g)
 
-    # Generate Children from Parents
-    children = []
-    for p in parents:
-        (_, child) = p
-        for c in range(0, len(child)):
-            if random.random() < RAND_RESET_PROB:
-                child[c] = random.randrange(0, 18)
-        children.append((my_cube.run_list(child), child))
-    print(children)
+        # Generate Children from Parents
+        children = []
+        for p in parents:
+            (_, child) = p
+            for c in range(0, len(child)):
+                if random.random() < RAND_RESET_PROB:
+                    child[c] = random.randrange(0, 18)
+            my_cube.cube_mat = og_state
+            children.append((my_cube.run_list(child), child))
+
+        # Update Population
+        population.sort()
+        children.sort(reverse=True)
+        for _ in range(0, int(len(children) / 2)):
+            population.pop(0)
+            population.append(children[0])
+            children.pop(0)
+    return population
 # end def
 
-gen_sga_sol()
+final_pop = gen_sga_sol()
+print(len(final_pop))
+for geno in final_pop:
+    print(geno)
