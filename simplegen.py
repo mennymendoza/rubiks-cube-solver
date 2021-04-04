7846dd9b48c40ee32245b36df272c80fb4249d08
mennymendoza <Juanmendoza97@mail.fresnostate.edu>
2021-04-04 14:39:08 -0700

    fixed comments/readability

--- simplegen.py
+++ simplegen.py
@@ -2,13 +2,20 @@ import rcube as cb
 import random
 import time
 
-# Constants
-NUM_SHUFFLES = 700 # Number of random operations done on cube in initial shuffle
-LIST_SIZE = 50 # Number of operations for each genotype
-POPULATION_SIZE = 100 # Fixed number of operations in population
-NUM_ITER = 5000 # Number of generations
-RAND_RESET_PROB = 0.2 # Probability of flipping an operation to a random operation
-PROP_CONSTANT = POPULATION_SIZE / 10 # Value multiplied to selection probability to increase/decrease chance of getting picked.
+# CONSTANTS
+# Number of Shuffles: Number of random operations done on cube in initial shuffling
+NUM_SHUFFLES = 700
+# List Size: Number of operations in each genotype
+LIST_SIZE = 50
+# Population Size: Number of genes in fixed population
+POPULATION_SIZE = 100
+# Number of Iterations: Number of generations
+NUM_ITER = 5000
+# Random Resest Probability: Probability of flipping an operation to another random operation
+RAND_RESET_PROB = 0.2
+# Proportionality Constant: Value multiplied to selection probability to increase/decrease
+# chance of a genotype getting picked to be a parent
+PROP_CONSTANT = POPULATION_SIZE / 10
 
 def gen_sga_sol():
     # Cube Initialization
@@ -28,7 +35,8 @@ def gen_sga_sol():
             genotype.append(random.randrange(0, 18))
         my_cube.cube_mat = og_state
         population.append((my_cube.run_list(genotype), genotype))
-
+    
+    # Main Loop
     for _ in range(0, NUM_ITER):
         # Select Parents
         parents = []
@@ -62,7 +70,14 @@ def gen_sga_sol():
     return population
 # end def
 
+start_time = time.time()
 final_pop = gen_sga_sol()
+exec_time = time.time() - start_time
 print(len(final_pop))
 for geno in final_pop:
     print(geno)
+print('Execution Time:', exec_time, 'seconds')
+
+
+
+
