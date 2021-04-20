086f9a31073e383b87e1149d483432a3cf61cb75
mennymendoza <Juanmendoza97@mail.fresnostate.edu>
2021-04-19 18:52:14 -0700

    tweaked params + comments

--- simanneal.py
+++ simanneal.py
@@ -11,11 +11,11 @@ NUM_SHUFFLES = 700
 # Iterations per Temperature
 ITER_PER_TEMP = 1
 # List Size
-LIST_SIZE = 100
+LIST_SIZE = 40
 # Number of Swaps
-NUM_SWAPS = 5
+NUM_SWAPS = 2
 # Number of Resets
-RAND_RESET_PROB = 0.05
+RAND_RESET_PROB = 0.002
 
 # Generate Simulated Annealing Solution
 def gen_sa_sol(init_temp):
@@ -23,24 +23,26 @@ def gen_sa_sol(init_temp):
     # Cube Initialization
     random.seed(time.time())
     my_cube = cb.RCube()
+
+    # Shuffling Cube
     for _ in range(0, NUM_SHUFFLES):
         my_cube.rotate(random.randrange(0, 18))
     shuff_state = my_cube.cube_mat
 
     # Initialize Solution
     op_list = []
-    for _ in range(0, LIST_SIZE):
-        op_list.append(random.randrange(0, 18))
+    for z in range(0, LIST_SIZE):
+        op_list.append(z % 18)
     solution = (my_cube.run_list(op_list), op_list)
 
     # Main Loop
     for t in range(0, TEMP_CYCLES):
-
+        # Prints t every 1000 cycles
         if t % 1000 == 0:
-            print(t)
+            print('t =', t)
 
         # Temp Function
-        temp = init_temp - (0.00004*t)
+        temp = init_temp - (0.00002*t)
         if (temp <= 0):
             print('Temperature frozen.')
             return
@@ -50,27 +52,29 @@ def gen_sa_sol(init_temp):
             (f0, old_list) = solution
             new_list = copy.deepcopy(old_list)
 
-            # Generate New Solution
+            # Generate new solution
             # Swap Mutation
             for _ in range(0, NUM_SWAPS):
-                idx1 = random.randrange(0, LIST_SIZE)
-                idx2 = random.randrange(0, LIST_SIZE)
-                tempo = new_list[idx1]
-                new_list[idx1] = new_list[idx2]
-                new_list[idx2] = tempo
+                idx = random.randrange(0, LIST_SIZE - 1)
+                temp_val = new_list[idx]
+                new_list[idx] = new_list[idx + 1]
+                new_list[idx + 1] = temp_val
             
             # Random Reset Mutation
             for z in range(0, LIST_SIZE):
                 if (random.random() < RAND_RESET_PROB):
                     new_list[z] = random.randrange(0, 18)
             
-            # Calculates New Fitness
+            # Calculates new fitness
             my_cube.cube_mat = shuff_state
             f1 = my_cube.run_list(new_list)
 
+            # Check to see if found solution
             if (f1 == 54):
                 print('SOLUTION FOUND at T =', init_temp)
                 return
+            
+            # Decides whether to accept new solution
             delta = f0 - f1
             prob_acc = (math.e)**(-delta/temp)
             if (f0 <= f1 or random.random() < prob_acc):
@@ -79,4 +83,5 @@ def gen_sa_sol(init_temp):
     print('No solution found at T =', init_temp)
 # end
 
-gen_sa_sol(5)
+
+gen_sa_sol(2)
