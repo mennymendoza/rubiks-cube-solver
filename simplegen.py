037cdd320fc9e32d1d235a173d8bfa8579d8c65f
mennymendoza <Juanmendoza97@mail.fresnostate.edu>
2021-04-19 23:19:57 -0700

    added swaps to sga

--- simplegen.py
+++ simplegen.py
@@ -1,6 +1,7 @@
 import rcube as cb
 import random
 import time
+import copy
 
 # TODO: add a check when fitness = 54
 # TODO: add crossovers
@@ -13,13 +14,15 @@ import time
 # Number of Shuffles: Number of random operations done on cube in initial shuffling
 NUM_SHUFFLES = 700
 # List Size: Number of operations in each genotype
-LIST_SIZE = 50
+LIST_SIZE = 22
 # Population Size: Number of genes in fixed population
-POPULATION_SIZE = 100
+POPULATION_SIZE = 1000
 # Number of Iterations: Number of generations
-NUM_ITER = 5000
+NUM_ITER = 25000
 # Random Resest Probability: Probability of flipping an operation to another random operation
-RAND_RESET_PROB = 0.2
+RAND_RESET_PROB = 0.15
+# Number of Swaps
+NUM_SWAPS = 3
 # Proportionality Constant: Value multiplied to selection probability to increase/decrease
 # chance of a genotype getting picked to be a parent
 PROP_CONSTANT = POPULATION_SIZE / 10
@@ -34,7 +37,6 @@ def gen_sga_sol():
     for _ in range(0, NUM_SHUFFLES):
         my_cube.rotate(random.randrange(0, 18))
     shuff_state = my_cube.cube_mat
-    my_cube.print_colors()
 
     # Generates Initial Population
     population = []
@@ -47,7 +49,8 @@ def gen_sga_sol():
         population.append((my_cube.run_list(genotype), genotype))
     
     # Main Loop
-    for _ in range(0, NUM_ITER):
+    for i in range(0, NUM_ITER):
+        print('Iter:', i)
         # Select Parents
         parents = []
         sum_f = 0
@@ -63,26 +66,32 @@ def gen_sga_sol():
         # Generate Children from Parents
         children = []
         for p in parents:
-            (_, child) = p
-            for c in range(0, len(child)):
+            (_, p_list) = p
+            child_list = copy.deepcopy(p_list)
+            # Random Reset Mutation
+            for c in range(0, len(child_list)):
                 if random.random() < RAND_RESET_PROB:
-                    child[c] = random.randrange(0, 18)
+                    child_list[c] = random.randrange(0, 18)
+            # Swap Mutation
+            for _ in range(0, NUM_SWAPS):
+                idx = random.randrange(0, LIST_SIZE - 1)
+                temp_val = child_list[idx]
+                child_list[idx] = child_list[idx + 1]
+                child_list[idx + 1] = temp_val
             my_cube.cube_mat = shuff_state
-            children.append((my_cube.run_list(child), child))
+            child_fit = my_cube.run_list(child_list)
+            if child_fit == 54:
+                print('Solution found!')
+                return (child_fit, child_list)
+            children.append((child_fit, child_list))
 
         # Update Population
         population.sort()
         children.sort(reverse=True)
-        for _ in range(0, int(len(children) / 2)):
+        for _ in range(0, len(children)):
             population.pop(0)
             population.append(children[0])
             children.pop(0)
-        
-        for gene in population:
-            (f, _) = gene
-            if f == 54:
-                print('Solution found!')
-                return population
     return population
 # end def
 
