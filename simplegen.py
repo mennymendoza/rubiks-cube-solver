1bacd9ce5bccc960980b1be606d049e0ffb5f473
mennymendoza <Juanmendoza97@mail.fresnostate.edu>
2021-04-25 23:57:25 -0700

    optimized solution space

--- simplegen.py
+++ simplegen.py
@@ -3,6 +3,7 @@ import random
 import time
 import copy
 import numpy as np
+import matplotlib.pyplot as plt
 
 # CONSTANTS
 # Number of Shuffles: Number of random operations done on cube in initial shuffling
@@ -13,13 +14,17 @@ LIST_SIZE = 50
 POPULATION_SIZE = 100
 # Number of Iterations: Number of generations
 NUM_ITER = 6000
-# Random Resest Probability: Probability of flipping an operation to another random operation
-RAND_RESET_PROB = 0.05
+# Random Resest Probability: Probability of chromosome mutating via Random Reset Mutation
+RAND_RESET_PROB = 0.4
+# Number of Resets
+NUM_RESETS = 5
 # Number of Swaps
 NUM_SWAPS = 5
 # Proportionality Constant: Value multiplied to selection probability to increase/decrease
 # chance of a genotype getting picked to be a parent
-PROP_CONSTANT = POPULATION_SIZE / 2
+PROP_CONSTANT = 1
+# Crossover Probability
+CROSSOVER_PROB = 0.8
 
 # Cube Object Initialization
 my_cube = cb.RCube()
@@ -34,18 +39,21 @@ def gen_sga_sol():
 
     # Generates Initial Population
     population = []
-    genotype = []
     for _ in range(0, POPULATION_SIZE):
-        genotype = []
-        for _ in range(0, LIST_SIZE):
-            genotype.append(random.randrange(0, 18))
+        genotype = [random.randrange(0, 18) for _ in range(0, POPULATION_SIZE)]
+        for idx in range(1, len(genotype) - 1):
+            if genotype[idx] % 2 == 0 and (genotype[idx] + 1 == genotype[idx - 1] or genotype[idx] + 1 == genotype[idx + 1]):
+                genotype[idx] = random.randrange(0, 18)
+            if genotype[idx] % 2 == 1 and (genotype[idx] - 1 == genotype[idx - 1] or genotype[idx] - 1 == genotype[idx + 1]):
+                genotype[idx] = random.randrange(0, 18)
         my_cube.cube_mat = shuff_state
         population.append((my_cube.run_list(genotype), genotype))
 
+    fitness_values = []
     # Main Loop
     for i in range(0, NUM_ITER):
         print('Iter:', i)
-        
+
         # Roulette Wheel Selection
         sum_f = 0
         for g in population:
@@ -56,37 +64,80 @@ def gen_sga_sol():
         mate_pool = [copy.deepcopy(population[idx]) for idx in mate_pool_idx]
 
         # Generate Children from Parents
-        children = []
-        for p in mate_pool:
-            (_, p_list) = p
-            child_list = copy.deepcopy(p_list)
+        for idx in range(int(len(mate_pool)/2)):
+            f0 = mate_pool[2 * idx][0]
+            f1 = mate_pool[2 * idx + 1][0]
+            list_1 = copy.deepcopy(mate_pool[2 * idx])[1]
+            list_2 = copy.deepcopy(mate_pool[2 * idx + 1])[1]
 
-            # Random Reset Mutation # TODO: use numpy
-            for c in range(len(child_list)):
-                if random.random() < RAND_RESET_PROB:
-                    child_list[c] = random.randrange(0, 18)
+            # Mutation / Recombination
+            if random.random() < CROSSOVER_PROB:
+                # Crossover Recombination
+                for z in range(random.randrange(len(list_1))):
+                    list_1[z], list_2[z] = list_2[z], list_1[z]
             
+            # Random Reset Mutation
+            if random.random() < RAND_RESET_PROB:
+                for _ in range(NUM_RESETS):
+                    list_1[random.randrange(len(list_1))] = random.randrange(0, 18)
+            if random.random() < RAND_RESET_PROB:
+                for _ in range(NUM_RESETS):
+                    list_2[random.randrange(len(list_2))] = random.randrange(0, 18)
+
             # Swap Mutation
             for _ in range(0, NUM_SWAPS):
-                idx = random.randrange(0, LIST_SIZE - 1)
-                child_list[idx], child_list[idx+1] = child_list[idx+1], child_list[idx]
-            
-            # Find fitness of child
+                idx = random.randrange(len(list_1) - 1)
+                list_1[idx], list_1[idx+1] = list_1[idx+1], list_1[idx]
+            for _ in range(0, NUM_SWAPS):
+                idx = random.randrange(len(list_2) - 1)
+                list_2[idx], list_2[idx+1] = list_2[idx+1], list_2[idx]
+
+            for j in range(1, len(list_1) - 1):
+                if list_1[j] % 2 == 0 and (list_1[j] + 1 == list_1[j - 1] or list_1[j] + 1 == list_1[j + 1]):
+                    list_1[j] = random.randrange(0, 18)
+                if list_1[j] % 2 == 1 and (list_1[j] - 1 == list_1[j - 1] or list_1[j] - 1 == list_1[j + 1]):
+                    list_1[j] = random.randrange(0, 18)
+            for j in range(1, len(list_2) - 1):
+                if list_2[j] % 2 == 0 and (list_2[j] + 1 == list_2[j - 1] or list_2[j] + 1 == list_2[j + 1]):
+                    list_2[j] = random.randrange(0, 18)
+                if list_2[j] % 2 == 1 and (list_2[j] - 1 == list_2[j - 1] or list_2[j] - 1 == list_2[j + 1]):
+                    list_2[j] = random.randrange(0, 18)
+
+            # BETA: trying to just accept different fitness values and let roulette eliminate less fit children
+            # Decide whether to add first child to mate pool
             my_cube.cube_mat = shuff_state
-            child_fit = my_cube.run_list(child_list)
-            if child_fit == 54:
-                print('Solution found!')
-                return (child_fit, child_list)
-            children.append((child_fit, child_list))
+            child_f0 = my_cube.run_list(list_1)
+            if child_f0 == 54:
+                print('Solution Found!')
+                return child_f0
+            elif child_f0 != f0 and child_f0 != f1:
+                mate_pool.append((child_f0, list_1))
+
+            # Decide whether to add second child to mate pool
+            my_cube.cube_mat = shuff_state
+            child_f1 = my_cube.run_list(list_2)
+            if child_f1 == 54:
+                print('Solution Found!')
+                return child_f1
+            elif child_f1 != f0 and child_f1 != f1:
+                mate_pool.append((child_f1, list_2))
+
+        # Adding top 10% of the population
+        population.sort(reverse=True)
+        for p in range(int(0.1 * len(population))):
+            mate_pool.append(copy.deepcopy(population[p]))
 
         # Update Population
-        population.sort()
-        children.sort(reverse=True)
-        for _ in range(0, int(len(children) / 2)):
-            population.pop(0)
-            population.append(children[0])
-            children.pop(0)
-        population.sort()
+        mate_pool.sort()
+        (max_fit, _) = mate_pool[-1]
+        print(max_fit)
+        fitness_values.append(max_fit)
+        if len(mate_pool) > POPULATION_SIZE:
+            for _ in range(len(mate_pool) - POPULATION_SIZE):
+                mate_pool.pop(0)
+        population = mate_pool
+    plt.plot(list(range(len(fitness_values))), fitness_values)
+    plt.show()
     return population
 # end def
 
